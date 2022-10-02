907a8618bc02c2f7b8c9241ba853a44b0e036cf9
Jiang Li <jiang@Jiangs-MacBook-Air.local>
2022-10-02 18:26:56 -0400

    add the job listing count by state

--- posts/job_listing/visual_job_listing.js
+++ posts/job_listing/visual_job_listing.js
@@ -18,25 +18,98 @@
     var path = d3.geoPath()
              .projection(d3.geoAlbersUsa());
 
-    d3.json("us-states.json", function(d) {
-        console.log("hello")
-        return d
-        //Bind data and create one path per GeoJSON feature
-        // svg.selectAll("path")
-        //    .data(json.features)
-        //    .enter()
-        //    .append("path")
-        //    .attr("d", path);
-    
-    }).then(function (json) {
-        console.log("hello")
-        svg.selectAll("path")
-           .data(json.features)
-           .enter()
-           .append("path")
-           .attr("d", path);
+    var color = d3.scaleQuantize()
+            .range(["rgb(237,248,233)", "rgb(186,228,179)",
+             "rgb(116,196,118)", "rgb(49,163,84)", "rgb(0,109,44)"]);
 
-    }).catch(function (error) {
-        console.log(error);
-      });
+    d3.csv("post_count_state.csv", function(d) {
+        return {
+            state: d.state, 
+            count:+d.count.replace(/,/g, '')
+        }
+    }).then(function(data) {
+        console.log()
+        color.domain([
+            d3.min(data, function(d) { return d.count; }),
+            d3.max(data, function(d) { return d.count; })
+        ]);
+        d3.json("us-states.json").then(function(json){
+        //Merge the ag. data and GeoJSON
+        // Loop through once for each ag. data value
+            for (var i = 0; i < data.length; i++) {
+    
+                //Grab state name
+                var dataState = data[i].state;
+    
+                //Grab data value, and convert from string to float
+                var dataValue = data[i].count;
+    
+                //Find the corresponding state inside the GeoJSON
+                for (var j = 0; j < json.features.length; j++) {
+    
+                var jsonState = json.features[j].properties.name;
+    
+                if (dataState == jsonState) {
+    
+                    //Copy the data value into the JSON
+                    json.features[j].properties.value = dataValue;
+    
+                    //Stop looking through the JSON
+                    break;
+                }
+    
+                }
+            }
+
+            // map
+            svg.selectAll("path")
+            .data(json.features)
+            .enter()
+            .append("path")
+            .attr("d", path)
+            .style("fill", function(d) {
+                //Get data value
+                var value = d.properties.value;
+                console.log(value)
+                if (value) {
+                    console.log(value)
+                    return color(value);
+                } else {
+                    //If value is undefined…
+                    return "#ccc";
+                }
+            })
+     
+
+
+
+        }) // json
+    }) // end csv
+
+    // d3.json("us-states.json", function(d) {
+    //     return d    
+    // }).then(function (json) {
+    //     svg.selectAll("path")
+    //        .data(json.features)
+    //        .enter()
+    //        .append("path")
+    //        .attr("d", path)
+    //        .style("fill", function(d) {
+    //            //Get data value
+    //            var value = d.properties.value;
+    //            console.log(value)
+    //            if (value) {
+    //                //If value exists…
+    //             //    console.log(value)
+    //                return color(value);
+    //            } else {
+    //                //If value is undefined…
+    //                return "#ccc";
+    //            }
+    //        })
+
+
+    // }).catch(function (error) {
+    //     console.log(error);
+    //   });
   
