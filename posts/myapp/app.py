f57706e8bd14588b0e45524f733c86db7cadb849
Jiang Li <jiang@Jiangs-MacBook-Air.local>
2023-08-01 22:40:27 -0400

    test stuent flow

---
+++ posts/myapp/app.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+import pandas
+from shiny import App, render, ui
+
+app_ui = ui.page_fluid(
+    ui.output_table("table"),
+)
+
+
+def server(input, output, session):
+    @output
+    @render.table
+    def table():
+        infile = Path(__file__).parent / "courses.csv"
+        df = pandas.read_csv(infile)
+        # Use the DataFrame's to_html() function to convert it to an HTML table, and
+        # then wrap with ui.HTML() so Shiny knows to treat it as raw HTML.
+        return df.head()
+
+
+app = App(app_ui, server)
